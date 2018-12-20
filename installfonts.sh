f40a468d53f4900cc98b891c51407b4af5076772
Alexander Kvamme <alexanderkvamme@gmail.com>
2018-12-20 20:09:55 +0100

    add some more files

---
+++ installfonts.sh
@@ -0,0 +1,15 @@
+brew tap caskroom/fonts
+FONTS=(
+	#font-inconsolidata
+	font-roboto
+	#font-clear-sans
+)
+brew cask install ${FONTS[@]}
+
+
+
+#andre:   # fonts
+ # tap "caskroom/fonts"
+ # cask "font-fira-code"
+ # cask "font-source-code-pro"
+
