ae3e093dd3b86c4689ac31c604b8c65d0c73ac15
Alexander Kvamme <alexanderkvamme@gmail.com>
2019-01-15 21:29:45 +0100

    add some Hammerspoon and Karabiner

---
+++ installers/installHammerspoon.sh
@@ -0,0 +1,9 @@
+brew cask install hammerspoon
+
+git clone https://github.com/jasonrudolph/keyboard.git ~/.keyboard
+
+cd ~/.keyboard
+
+script/setup
+
+
---
+++ installers/installKarabiner.sh
@@ -0,0 +1,5 @@
+brew cask install karabiner-elements
+
+open karabiner://karabiner/assets/complex_modifications/import?url=https://pqrs.org/osx/karabiner/complex_modifications/json/space_cadet.json
+
+
