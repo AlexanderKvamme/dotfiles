5181aeea49ef6b107f45a0704fee81067e4ac9d6
Alexander Kvamme <alexanderkvamme@gmail.com>
2019-01-17 18:04:15 +0100

    make keyboard config symlink to dotfiles

--- installers/installKarabiner.sh
+++ installers/installKarabiner.sh
@@ -1,3 +1,5 @@
 brew cask install karabiner-elements
 
 open karabiner://karabiner/assets/complex_modifications/import?url=https://raw.githubusercontent.com/AlexanderKvamme/dotfiles/develop/resources/Karabiner/karabinalf.json
+
+ln -s ~/.config/karabiner/ ~/.dotfiles/resources/karabinerRes/
