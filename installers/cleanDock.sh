20812892be0e61ea4b3d29ceacaaa99f114be078
Alexander Kvamme <alexanderkvamme@gmail.com>
2022-07-26 17:38:44 +0200

    Clean dock

---
+++ installers/cleanDock.sh
@@ -0,0 +1,15 @@
+brew install --cask hpedrorodrigues/tools/dockutil
+
+# Clean up and remove dock items
+dockutil --remove 'Launchpad'
+dockutil --remove 'Safari'
+dockutil --remove 'Maps'
+dockutil --remove 'Photos'
+dockutil --remove 'FaceTime'
+dockutil --remove 'Calendar'
+dockutil --remove 'Podcasts'
+dockutil --remove 'Notes'
+dockutil --remove 'TV'
+dockutil --remove 'Music'
+dockutil --remove 'System Preferences'
+dockutil --remove 'App Store'
