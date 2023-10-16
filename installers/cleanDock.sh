a8bdcfc6319cab9110c4ffbd910a1273181e2584
Alexander Kvamme <alexanderkvamme@gmail.com>
2023-10-16 12:46:24 +0200

    Update cleanDock

--- installers/cleanDock.sh
+++ installers/cleanDock.sh
@@ -2,6 +2,7 @@ brew install --cask hpedrorodrigues/tools/dockutil
 
 # Clean up and remove dock items
 dockutil --remove 'Launchpad'
+dockutil --remove 'Messages'
 dockutil --remove 'Safari'
 dockutil --remove 'Maps'
 dockutil --remove 'Photos'
@@ -13,3 +14,8 @@ dockutil --remove 'TV'
 dockutil --remove 'Music'
 dockutil --remove 'System Preferences'
 dockutil --remove 'App Store'
+dockutil --remove 'Mail'
+dockutil --remove 'Contacts'
+dockutil --remove 'Reminders'
+dockutil --remove 'Freeform'
+dockutil --remove 'System Settings'
